9a21dffaa1c299ec5e983f74b14fbfe51fd0ad20
Sanket Patil <sanketpatil9902@gmail.com>
2024-08-23 00:55:59 +0530

    Hello world using react

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const title = React.createElement("h1", {id:"h1"}, "This is h1 heading");
+const title = React.createElement("h1", { id: "h1" }, "This is h1 heading");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(title);
