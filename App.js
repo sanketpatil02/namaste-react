4ce75207b6d7d7216eedef6c5c6699af2cfcdd71
Your Name <your_email@example.com>
2024-09-03 23:10:30 +0530

    Ep-04 (continuing)

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element
-const title = <h1 className="head">Namaste react🚀 using JSX</h1>;
+/*
+Header
+ -Logo
+ -Nav Items
+Body
+ -Search
+ -RestaurantContainer
+  -RestaurantCard
+Footer
+ -Copyright
+ -Link
+ -Address
+ -Contact
+*/
 
-// alternate syntax
-const HeadingComponent = () => (
-  <div className="container">
-    {/* this is component composition */}
-    {title}
-    <h1 className="heading">Namaste react🚀 functional component</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/736x/b0/1e/69/b01e69dfd04399324803c453b0fb9029.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
