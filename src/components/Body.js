95daa5b0e8aba39a3857ab757508933b5a60e0eb
Your Name <your_email@example.com>
2024-10-17 00:18:07 +0530

    episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,69 +1,65 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  let restaurantL = [
-    {
-      info: {
-        id: "456986",
-        name: "Domino's Pizza",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/13/ae6581d6-a1b5-449a-916e-23c50575794c_456986.JPG",
-        locality: "Somwar Peth",
-        areaName: "Rasta Peth",
-        costForTwo: "₹400 for two",
-        cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
-        avgRating: 3.8,
-        sla: {
-          deliveryTime: 25,
-        },
-      },
-    },
-    {
-      info: {
-        id: "456987",
-        name: "KFC",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/13/ae6581d6-a1b5-449a-916e-23c50575794c_456986.JPG",
-        locality: "Somwar Peth",
-        areaName: "Rasta Peth",
-        costForTwo: "₹400 for two",
-        cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
-        avgRating: 4.5,
-        sla: {
-          deliveryTime: 25,
-        },
-      },
-    },
-    {
-      info: {
-        id: "456988",
-        name: "MacD",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/13/ae6581d6-a1b5-449a-916e-23c50575794c_456986.JPG",
-        locality: "Somwar Peth",
-        areaName: "Rasta Peth",
-        costForTwo: "₹400 for two",
-        cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
-        avgRating: 4.1,
-        sla: {
-          deliveryTime: 25,
-        },
-      },
-    },
-  ];
-  // console.log(restaurantL);
-  const [topRestaurant, setTopRestaurant] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState();
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+    console.log(jsonData);
+    setListOfRestaurants(
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+
+    setFilteredRestaurants(
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              // console.log(filteredList);
+
+              setFilteredRestaurants(filteredList);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
-          className="top-res"
+          className="filter-btn"
           onClick={() => {
-            setTopRestaurant(
-              topRestaurant.filter((res) => res.info.avgRating > 4)
+            setListOfRestaurants(
+              listOfRestaurants.filter((res) => res.info.avgRating > 4)
             );
           }}
         >
@@ -71,7 +67,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {topRestaurant.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
